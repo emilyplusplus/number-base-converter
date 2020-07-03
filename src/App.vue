1cb1c30f1804b0c5f9d31cb8259c36cdd05ef9cc
Emily Carlsen <emilytcarlsen@gmail.com>
2020-07-03 18:06:17 -0400

    Add grid layout for calculator

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,102 @@
 <template>
   <v-app>
     <v-content>
-      
+      <v-container fill-height fluid>
+        <v-row align="center" justify="center">
+          <v-col>
+            <v-card
+              class="mx-auto"
+              max-width="400"
+              height="600"
+            >
+              <v-container class="grey lighten-5">
+                <v-col cols="12" sm="6">
+                  <v-text-field
+                    label="Regular"
+                    single-line
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-text-field
+                    label="Regular"
+                    single-line
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-text-field
+                    label="Regular"
+                    single-line
+                  ></v-text-field>
+                </v-col>
+                <v-row no-gutters>
+                  <v-col
+                    v-for="n in 3"
+                    :key="n"
+                    cols="12"
+                    sm="4"
+                  >
+                    <v-card
+                      class="pa-2"
+                      outlined
+                      tile
+                    >
+                      One of three columns
+                    </v-card>
+                  </v-col>
+                </v-row>
+                <v-row no-gutters>
+                  <v-col
+                    v-for="n in 3"
+                    :key="n"
+                    cols="12"
+                    sm="4"
+                  >
+                    <v-card
+                      class="pa-2"
+                      outlined
+                      tile
+                    >
+                      One of three columns
+                    </v-card>
+                  </v-col>
+                </v-row>
+                <v-row no-gutters>
+                  <v-col
+                    v-for="n in 3"
+                    :key="n"
+                    cols="12"
+                    sm="4"
+                  >
+                    <v-card
+                      class="pa-2"
+                      outlined
+                      tile
+                    >
+                      One of three columns
+                    </v-card>
+                  </v-col>
+                </v-row>
+                <v-row no-gutters>
+                  <v-col
+                    v-for="n in 3"
+                    :key="n"
+                    cols="12"
+                    sm="4"
+                  >
+                    <v-card
+                      class="pa-2"
+                      outlined
+                      tile
+                    >
+                      One of three columns
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
