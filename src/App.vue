65ebf1ef0603683533d1b18a7f4127b15b9d725b
Emily Carlsen <emilytcarlsen@gmail.com>
2020-07-03 18:30:09 -0400

    Add buttons

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,88 @@
 <template>
   <v-app>
-    <v-content>
+    <v-main>
       <v-container fill-height fluid>
         <v-row align="center" justify="center">
           <v-col>
             <v-card
-              class="mx-auto over-hidden"
+              class="mx-auto over-hidden grey darken-3"
               max-width="400"
-              height="600"
+              height="700"
+              elevation="12"
             >
-              <v-container class="grey lighten-5">
-                <v-col cols="12">
-                  <v-text-field
-                    label="Hexadecimal"
-                    single-line
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field
-                    label="Decimal"
-                    single-line
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field
-                    label="Octal"
-                    single-line
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field
-                    label="Binary"
-                    single-line
-                  ></v-text-field>
-                </v-col>
+              <v-container class="white">
                 <v-row no-gutters>
-                  <v-col
-                    v-for="n in 3"
-                    :key="n"
-                    cols="12"
-                    sm="4"
-                  >
-                    <v-card
-                      class="pa-2"
-                      outlined
-                      tile
-                    >
-                      One of three columns
-                    </v-card>
+                  <v-col cols="12">
+                    <div style="height: 225px;"></div>
                   </v-col>
                 </v-row>
                 <v-row no-gutters>
-                  <v-col
-                    v-for="n in 3"
-                    :key="n"
-                    cols="12"
-                    sm="4"
-                  >
-                    <v-card
-                      class="pa-2"
-                      outlined
-                      tile
-                    >
-                      One of three columns
-                    </v-card>
+                  <v-col cols="12">
+                    
                   </v-col>
                 </v-row>
                 <v-row no-gutters>
-                  <v-col
-                    v-for="n in 3"
-                    :key="n"
-                    cols="12"
-                    sm="4"
-                  >
-                    <v-card
-                      class="pa-2"
-                      outlined
-                      tile
-                    >
-                      One of three columns
-                    </v-card>
+                  <v-col cols="12">
+                    
                   </v-col>
                 </v-row>
                 <v-row no-gutters>
-                  <v-col
-                    v-for="n in 3"
-                    :key="n"
-                    cols="12"
-                    sm="4"
-                  >
-                    <v-card
-                      class="pa-2"
-                      outlined
-                      tile
-                    >
-                      One of three columns
-                    </v-card>
+                  <v-col cols="12">
+                    
                   </v-col>
                 </v-row>
               </v-container>
+              <v-container class="grey darken-3">
+                  <v-row no-gutters>
+                    <v-col
+                      v-for="n in 4"
+                      :key="n"
+                      cols="3"
+                    >
+                      <v-button @click.stop icon>
+                        A
+                      </v-button>
+                    </v-col>
+                  </v-row>
+                  <v-row no-gutters>
+                    <v-col
+                      v-for="n in 4"
+                      :key="n"
+                      cols="3"
+                    >
+                      <v-button icon>
+                        7
+                      </v-button>
+                    </v-col>
+                  </v-row>
+                  <v-row no-gutters>
+                    <v-col
+                      v-for="n in 4"
+                      :key="n"
+                      cols="3"
+                    >
+                      <v-button icon>
+                        4
+                      </v-button>
+                    </v-col>
+                  </v-row>
+                  <v-row no-gutters>
+                    <v-col
+                      v-for="n in 4"
+                      :key="n"
+                      cols="3"
+                    >
+                      <v-button icon>
+                        0
+                      </v-button>
+                    </v-col>
+                  </v-row>
+                </v-container>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
