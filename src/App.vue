70ed3d367343ebd2beec925f738cfbd6a57714b8
Emily Carlsen <emilytcarlsen@gmail.com>
2020-07-03 18:47:18 -0400

    Add keyboard

--- src/App.vue
+++ src/App.vue
@@ -32,49 +32,53 @@
                   </v-col>
                 </v-row>
               </v-container>
-              <v-container class="grey darken-3">
+              <v-container class="grey darken-3 pt-8">
                   <v-row no-gutters>
                     <v-col
                       v-for="n in 4"
-                      :key="n"
+                      :key="key(n+11)"
                       cols="3"
+                      align="center"
                     >
-                      <v-button @click.stop icon>
-                        A
-                      </v-button>
+                      <v-btn x-large="" class="white--text" @click.stop="press(key(n+11))" icon>
+                        {{ key(n+11) }}
+                      </v-btn>
                     </v-col>
                   </v-row>
-                  <v-row no-gutters>
+                  <v-row class="mt-2" no-gutters>
                     <v-col
                       v-for="n in 4"
-                      :key="n"
+                      :key="key(n+11)"
                       cols="3"
+                      align="center"
                     >
-                      <v-button icon>
-                        7
-                      </v-button>
+                      <v-btn x-large="" class="white--text" @click.stop="press(key(n+7))" icon>
+                        {{ key(n+7) }}
+                      </v-btn>
                     </v-col>
                   </v-row>
-                  <v-row no-gutters>
+                  <v-row class="mt-2" no-gutters>
                     <v-col
                       v-for="n in 4"
-                      :key="n"
+                      :key="key(n+11)"
                       cols="3"
+                      align="center"
                     >
-                      <v-button icon>
-                        4
-                      </v-button>
+                      <v-btn x-large="" class="white--text" @click.stop="press(key(n+3))" icon>
+                        {{ key(n+3) }}
+                      </v-btn>
                     </v-col>
                   </v-row>
-                  <v-row no-gutters>
+                  <v-row class="mt-2" no-gutters>
                     <v-col
                       v-for="n in 4"
-                      :key="n"
+                      :key="key(n+11)"
                       cols="3"
+                      align="center"
                     >
-                      <v-button icon>
-                        0
-                      </v-button>
+                      <v-btn x-large="" class="white--text" @click.stop="press(key(n-1))" icon>
+                        {{ key(n-1) }}
+                      </v-btn>
                     </v-col>
                   </v-row>
                 </v-container>
@@ -97,6 +101,23 @@ export default {
   data: () => ({
     
   }),
+
+  methods: {
+    noop() {
+      console.log('noop')
+    },
+    key(i) {
+      if(i > 9) {
+        return i.toString(16)
+      }
+      return i.toString()
+    },
+    press(key) {
+      console.log(key)
+    }
+  },
+
+  
 };
 </script>
 
